91f5434452951a89ec1f6e43be2d2e5ea7d034e8
Valmir Jr <valmir.junior@ee.ufcg.edu.br>
2020-01-05 08:24:51 -0300

    update: sign in authentication

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -1,9 +1,18 @@
+import produce from "immer";
+
 const INITIAL_SATE = {
-  token: true,
+  token: null,
+  signed: false,
+  loading: false,
 };
 
 export default function auth(state = INITIAL_SATE, action) {
   switch (action.type) {
+    case "@auth/SIGN_IN_SUCCESS":
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }
