91548fffed2df057065a2d1060c479492cf6b913
Valmir Jr <valmir.junior@ee.ufcg.edu.br>
2020-01-20 01:27:20 -0300

    chore: edit default layout

---
+++ src/pages/_layouts/edit/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import PropTypes from "prop-types";
+import { Container } from "./styles";
+
+export default function EditLayout({ children }) {
+  return <Container>{children}</Container>;
+}
+
+EditLayout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
